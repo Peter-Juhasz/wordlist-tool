b2199a12c61e97c438fa8358b9d095386e400118
Juhász Péter <peter.juhasz@live.com>
2023-02-05 15:36:07 +0100

    fix

--- src/WordlistTool.Core/Serialization/WordlistWriter.cs
+++ src/WordlistTool.Core/Serialization/WordlistWriter.cs
@@ -15,21 +15,21 @@ public static class WordlistWriter
 		}
 	}
 
-	public static async Task WriteAsync(Stream stream, Encoding encoding, byte[] lineEnding, IEnumerable<string> list, int bufferSize, CancellationToken cancellationToken)
+	public static async Task WriteAsync(Stream stream, int bufferSize, Encoding encoding, byte[] lineEnding, IEnumerable<string> list, CancellationToken cancellationToken)
 	{
-		using var writer = new StreamWriter(stream, encoding, bufferSize) { NewLine = Encoding.ASCII.GetString(lineEnding) };
+		using var writer = new StreamWriter(stream, encoding, bufferSize) { NewLine = encoding.GetString(lineEnding) };
 		await WriteAsync(writer, list, cancellationToken);
 	}
 
-	public static async Task WriteAsync(string filePath, Encoding encoding, byte[] lineEnding, IEnumerable<string> list, int bufferSize, CancellationToken cancellationToken)
+	public static async Task WriteAsync(string filePath, int bufferSize, Encoding encoding, byte[] lineEnding, IEnumerable<string> list, CancellationToken cancellationToken)
 	{
 		await using var stream = File.Create(filePath, bufferSize);
-		await WriteAsync(stream, encoding, lineEnding, list, bufferSize, cancellationToken);
+		await WriteAsync(stream, bufferSize, encoding, lineEnding, list, cancellationToken);
 	}
 
 	public static async Task WriteAsync(OutputOptions output, IEnumerable<string> list, CancellationToken cancellationToken)
 	{
-		await WriteAsync(output.Stream, output.Encoding, output.LineEndingBytes, list, output.BufferSize, cancellationToken);
+		await WriteAsync(output.Stream, output.BufferSize, output.Encoding, output.LineEndingBytes, list, cancellationToken);
 	}
 
 
@@ -43,40 +43,40 @@ public static class WordlistWriter
 		}
 	}
 
-	public static async Task WriteAsync(Stream stream, Encoding encoding, byte[] lineEnding, IAsyncEnumerable<string> list, int bufferSize, CancellationToken cancellationToken)
+	public static async Task WriteAsync(Stream stream, int bufferSize, Encoding encoding, byte[] lineEnding, IAsyncEnumerable<string> list, CancellationToken cancellationToken)
 	{
-		using var writer = new StreamWriter(stream, encoding, bufferSize) { NewLine = Encoding.ASCII.GetString(lineEnding) };
+		using var writer = new StreamWriter(stream, encoding, bufferSize) { NewLine = encoding.GetString(lineEnding) };
 		await WriteAsync(writer, list, cancellationToken);
 	}
 
-	public static async Task WriteAsync(string filePath, Encoding encoding, byte[] lineEnding, IAsyncEnumerable<string> list, int bufferSize, CancellationToken cancellationToken)
+	public static async Task WriteAsync(string filePath, int bufferSize, Encoding encoding, byte[] lineEnding, IAsyncEnumerable<string> list, CancellationToken cancellationToken)
 	{
 		await using var stream = File.Create(filePath, bufferSize);
-		await WriteAsync(stream, encoding, lineEnding, list, bufferSize, cancellationToken);
+		await WriteAsync(stream, bufferSize, encoding, lineEnding, list, cancellationToken);
 	}
 
 	public static async Task WriteAsync(OutputOptions output, IAsyncEnumerable<string> list, CancellationToken cancellationToken)
 	{
-		await WriteAsync(output.Stream, output.Encoding, output.LineEndingBytes, list, output.BufferSize, cancellationToken);
+		await WriteAsync(output.Stream, output.BufferSize, output.Encoding, output.LineEndingBytes, list, cancellationToken);
 	}
 
 
 	public static StringLineWriter GetWriterAsync(TextWriter writer) => new(writer);
 
-	public static StringLineWriter GetWriterAsync(Stream stream, Encoding encoding, byte[] lineEnding, int bufferSize)
+	public static StringLineWriter GetWriterAsync(Stream stream, int bufferSize, Encoding encoding, byte[] lineEnding)
 	{
-		var streamWriter = new StreamWriter(stream, encoding, bufferSize) { NewLine = Encoding.ASCII.GetString(lineEnding) };
+		var streamWriter = new StreamWriter(stream, encoding, bufferSize) { NewLine = encoding.GetString(lineEnding) };
 		return new(streamWriter);
 	}
 
 	public static StringLineWriter GetWriterAsync(OutputOptions output)
 	{
-		return GetWriterAsync(output.Stream, output.Encoding, output.LineEndingBytes, output.BufferSize);
+		return GetWriterAsync(output.Stream, output.BufferSize, output.Encoding, output.LineEndingBytes);
 	}
 
-	public static async Task<StringLineWriter> GetWriterAsync(string filePath, Encoding encoding, byte[] lineEnding, int bufferSize, CancellationToken cancellationToken)
+	public static async Task<StringLineWriter> GetWriterAsync(string filePath, int bufferSize, Encoding encoding, byte[] lineEnding, CancellationToken cancellationToken)
 	{
 		var stream = File.Create(filePath);
-		return GetWriterAsync(stream, encoding, lineEnding, bufferSize);
+		return GetWriterAsync(stream, bufferSize, encoding, lineEnding);
 	}
 }
